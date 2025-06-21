b47a3910f0a11e3bd48a7c79307b50d86e4e01f4
Daniel Pravutiner <62667601+danprav04@users.noreply.github.com>
2025-06-21 21:48:49 +0300

    again

--- my-loan-app/app/constants.ts
+++
@@ -1,8 +0,0 @@
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://your-production-domain.com' 
-    : 'http://localhost:3000';
-
-if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined in the environment variables");
-}
